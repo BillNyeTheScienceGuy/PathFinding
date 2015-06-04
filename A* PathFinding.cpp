885c9fb53e67281a351f32da176796a46af64e38
BillNyeTheScienceGuy <BillNyeTheScienceGuy@users.noreply.github.com>
2015-06-04 13:00:10 -0800

    Update A* PathFinding.cpp

--- A* PathFinding.cpp
+++ A* PathFinding.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
-#include <limits>    // to set int to max value
+#include <limits>	// to set int to max value
  
 using namespace std;
  
-#define SIZE 10
+#define SIZE 10	// upper limit: 159; lower limit: 2
  
 int startx = 0, starty = 0, startd = 0;
-int stopx = SIZE - 1, stopy = 0;
+int stopx = SIZE - 1, stopy = SIZE - 1;
+int hfactor = 10;	// higher the factor, faster the solution but not guaranteed to be the shortest solution
  
-static int dmap[4][4] = { {3, 2, 1, 2}, {2, 3, 2, 1}, {1, 2, 3, 2}, {2, 1, 2, 3} };      // maps move costs with directions
+static int dmap[4][4] = { {3, 2, 1, 2}, {2, 3, 2, 1}, {1, 2, 3, 2}, {2, 1, 2, 3} };	// maps move costs with directions
  
 class node {
 	bool walkable;	// walkability
@@ -54,7 +55,7 @@ class node {
 		void setxpyp(int xi, int yi) {
 			xp = xi;
 			yp = yi;
-			d = ((x - xi) > 0) + 2*((y - yi) < 0) + 3*((x - xi) < 0);	// set direction to point at parent
+			d = ((xi - x) > 0) + 2*((yi - y) < 0) + 3*((xi - x) < 0);	// set direction to point at parent
 		}
 		void setstatus(int s) { status = s; }
 		void calcg(node f) {
@@ -64,9 +65,24 @@ class node {
 		}
 		void calch(int xf, int yf) {
 			h = abs(xf - x) + abs(yf - y);	// 
-			int dir = ((xf - x) > 0) + 2*((yf - y) < 0) + 3*((xf - x) < 0);	// direction to next node
-			dir = (dir - d + 4)%4;
-
+			int dir;	// direction value to next node
+			switch ((d + 2)%4) {	// direction will be facing in node
+				case 0:
+					dir = 0*((yf - y) > 0) + 1*((xf - x) > 0) + 2*((yf - y) < 0) + 1*((xf - x) < 0);
+					break;
+				case 1:
+					dir = 1*((yf - y) > 0) + 0*((xf - x) > 0) + 1*((yf - y) < 0) + 2*((xf - x) < 0);
+					break;
+				case 2:
+					dir = 2*((yf - y) > 0) + 1*((xf - x) > 0) + 0*((yf - y) < 0) + 1*((xf - x) < 0);
+					break;
+				case 3:
+					dir = 1*((yf - y) > 0) + 2*((xf - x) > 0) + 1*((yf - y) < 0) + 0*((xf - x) < 0);
+					break;
+			}
+			if (dir == 3) { dir = 2; }
+			h += dir;
+			h *= hfactor;
 		}
 };
  
@@ -87,7 +103,7 @@ void print(node n[SIZE][SIZE]) {
 		}
 		cout << endl << " |\t";
 		for (int j = 0; j < SIZE; j++) {
-			cout << n[j][i].getxp() << "," << n[j][i].getyp() << "\t";	// parent coords
+			cout << n[j][i].getxp() << "," << n[j][i].getyp() << "," << n[j][i].getd() << "\t";	// parent coords
 		}
 		cout << endl << " |\t";
 		for (int j = 0; j < SIZE; j++) {
@@ -117,9 +133,9 @@ void lowestF(node n[SIZE][SIZE], int &x, int &y) {
 	for (int i = 0; i < SIZE; i++) {
 		for (int j = 0; j < SIZE; j++) {
 			if (n[i][j].iswalkable() && n[i][j].getstatus() == 1 && n[i][j].getf() < lowf) {	// if walkable and on open list and lower than lowest F encountered
-			      lowf = n[i][j].getf();
-			      x = i;
-			      y = j;
+				lowf = n[i][j].getf();
+				x = i;
+				y = j;
 			}
 		}
 	}
@@ -143,97 +159,115 @@ void main() {
  
 		if (curx + 1 < SIZE) {	// if there's a node to the right
 			if (n[curx + 1][cury].iswalkable()) {	// if the node is walkable
-			      switch (n[curx + 1][cury].getstatus()) {
-				      case 1:	// if the node is in the open list
-					      oldg = n[curx + 1][cury].getg();	// save the old G score for comparison
-					      n[curx + 1][cury].calcg(n[curx][cury]);	// recalculate a new G score
-					      if (n[curx + 1][cury].getg() >= oldg) {	// if the new G score is not less than the old
-						      n[curx + 1][cury].setg(oldg);	// set the score back to the old
-					      }
-					      else {
-						      n[curx + 1][cury].setxpyp(curx, cury);	// else set the node's parent to current node
-						      n[curx + 1][cury].calch(stopx, stopy);	// recalculate the H score
-					      }
-					      break;
-				      case 0:	// if the node is not in a list
-					      n[curx + 1][cury].setstatus(1);	// place in open list
-					      n[curx + 1][cury].calcg(n[curx][cury]);	// calculate the G score
-					      n[curx + 1][cury].calch(stopx, stopy);	// calculate the H score
-					      n[curx + 1][cury].setxpyp(curx, cury);	// set the node's parent to current node
-					      break;
-			      }
+				switch (n[curx + 1][cury].getstatus()) {
+					case 1:	// if the node is in the open list
+						oldg = n[curx + 1][cury].getg();	// save the old G score for comparison
+						n[curx + 1][cury].calcg(n[curx][cury]);	// recalculate a new G score
+						if (n[curx + 1][cury].getg() >= oldg) {	// if the new G score is not less than the old
+							n[curx + 1][cury].setg(oldg);	// set the score back to the old
+						}
+						else {
+							n[curx + 1][cury].setxpyp(curx, cury);	// else set the node's parent to current node
+							n[curx + 1][cury].calch(stopx, stopy);	// recalculate the H score
+						}
+						break;
+					case 0:	// if the node is not in a list
+						n[curx + 1][cury].setstatus(1);	// place in open list
+						n[curx + 1][cury].calcg(n[curx][cury]);	// calculate the G score
+						n[curx + 1][cury].calch(stopx, stopy);	// calculate the H score
+						n[curx + 1][cury].setxpyp(curx, cury);	// set the node's parent to current node
+						break;
+				}
 			}
 		}
 		if (curx > 0) {	// if there's a node to the left
 			if (n[curx - 1][cury].iswalkable()) {	// if the node is walkable
-			      switch (n[curx - 1][cury].getstatus()) {
-				      case 1:	// if the node is in the open list
-					      oldg = n[curx - 1][cury].getg();	// save the old G score for comparison
-					      n[curx - 1][cury].calcg(n[curx][cury]);	// recalculate a new G score
-					      if (n[curx - 1][cury].getg() >= oldg) {	// if the new G score is not less than the old
-						      n[curx - 1][cury].setg(oldg);	// set the score back to the old
-					      }
-					      else {
-						      n[curx - 1][cury].setxpyp(curx, cury);	// else set the node's parent to current node
-						      n[curx - 1][cury].calch(stopx, stopy);	// recalculate the H score
-					      }
-					      break;
-				      case 0:	// if the node is not in a list
-					      n[curx - 1][cury].setstatus(1);	// place in open list
-					      n[curx - 1][cury].calcg(n[curx][cury]);	// calculate the G score
-					      n[curx - 1][cury].calch(stopx, stopy);	// calculate the H score
-					      n[curx - 1][cury].setxpyp(curx, cury);	// set the node's parent to current node
-					      break;
-			      }
+				switch (n[curx - 1][cury].getstatus()) {
+					case 1:	// if the node is in the open list
+						oldg = n[curx - 1][cury].getg();	// save the old G score for comparison
+						n[curx - 1][cury].calcg(n[curx][cury]);	// recalculate a new G score
+						if (n[curx - 1][cury].getg() >= oldg) {	// if the new G score is not less than the old
+							n[curx - 1][cury].setg(oldg);	// set the score back to the old
+						}
+						else {
+							n[curx - 1][cury].setxpyp(curx, cury);	// else set the node's parent to current node
+							n[curx - 1][cury].calch(stopx, stopy);	// recalculate the H score
+						}
+						break;
+					case 0:	// if the node is not in a list
+						n[curx - 1][cury].setstatus(1);	// place in open list
+						n[curx - 1][cury].calcg(n[curx][cury]);	// calculate the G score
+						n[curx - 1][cury].calch(stopx, stopy);	// calculate the H score
+						n[curx - 1][cury].setxpyp(curx, cury);	// set the node's parent to current node
+						break;
+				}
 			}
 		}
 		if (cury + 1 < SIZE) {	// if there's a node above
 			if (n[curx][cury + 1].iswalkable()) {	// if the node is walkable
-			      switch (n[curx][cury + 1].getstatus()) {
-				      case 1:	// if the node is in the open list
-					      oldg = n[curx][cury + 1].getg();	// save the old G score for comparison
-					      n[curx][cury + 1].calcg(n[curx][cury]);	// recalculate a new G score
-					      if (n[curx][cury + 1].getg() >= oldg) {	// if the new G score is not less than the old
-						      n[curx][cury + 1].setg(oldg);	// set the score back to the old
-					      }
-					      else {
-						      n[curx][cury + 1].setxpyp(curx, cury);	// else set the node's parent to current node
-						      n[curx][cury + 1].calch(stopx, stopy);	// recalculate the H score
-					      }
-					      break;
-				      case 0:	// if the node is not in a list
-					      n[curx][cury + 1].setstatus(1);	// place in open list
-					      n[curx][cury + 1].calcg(n[curx][cury]);	// calculate the G score
-					      n[curx][cury + 1].calch(stopx, stopy);	// calculate the H score
-					      n[curx][cury + 1].setxpyp(curx, cury);	// set the node's parent to current node
-					      break;
-			      }
+				switch (n[curx][cury + 1].getstatus()) {
+					case 1:	// if the node is in the open list
+						oldg = n[curx][cury + 1].getg();	// save the old G score for comparison
+						n[curx][cury + 1].calcg(n[curx][cury]);	// recalculate a new G score
+						if (n[curx][cury + 1].getg() >= oldg) {	// if the new G score is not less than the old
+							n[curx][cury + 1].setg(oldg);	// set the score back to the old
+						}
+						else {
+							n[curx][cury + 1].setxpyp(curx, cury);	// else set the node's parent to current node
+							n[curx][cury + 1].calch(stopx, stopy);	// recalculate the H score
+						}
+						break;
+					case 0:	// if the node is not in a list
+						n[curx][cury + 1].setstatus(1);	// place in open list
+						n[curx][cury + 1].calcg(n[curx][cury]);	// calculate the G score
+						n[curx][cury + 1].calch(stopx, stopy);	// calculate the H score
+						n[curx][cury + 1].setxpyp(curx, cury);	// set the node's parent to current node
+						break;
+				}
 			}
 		}
 		if (cury > 0) {	// if there's a node below
 			if (n[curx][cury - 1].iswalkable()) {	// if the node is walkable
-			      switch (n[curx][cury - 1].getstatus()) {
-				      case 1:	// if the node is in the open list
-					      oldg = n[curx][cury - 1].getg();	// save the old G score for comparison
-					      n[curx][cury - 1].calcg(n[curx][cury]);	// recalculate a new G score
-					      if (n[curx][cury - 1].getg() >= oldg) {	// if the new G score is not less than the old
-						      n[curx][cury - 1].setg(oldg);	// set the score back to the old
-					      }
-					      else {
-						      n[curx][cury - 1].setxpyp(curx, cury);	// else set the node's parent to current node
-						      n[curx][cury - 1].calch(stopx, stopy);	// recalculate the H score
-					      }
-					      break;
-				      case 0:	// if the node is not in a list
-					      n[curx][cury - 1].setstatus(1);	// place in open list
-					      n[curx][cury - 1].calcg(n[curx][cury]);	// calculate the G score
-					      n[curx][cury - 1].calch(stopx, stopy);	// calculate the H score
-					      n[curx][cury - 1].setxpyp(curx, cury);	// set the node's parent to current node
-					      break;
-			      }
+				switch (n[curx][cury - 1].getstatus()) {
+					case 1:	// if the node is in the open list
+						oldg = n[curx][cury - 1].getg();	// save the old G score for comparison
+						n[curx][cury - 1].calcg(n[curx][cury]);	// recalculate a new G score
+						if (n[curx][cury - 1].getg() >= oldg) {	// if the new G score is not less than the old
+							n[curx][cury - 1].setg(oldg);	// set the score back to the old
+						}
+						else {
+							n[curx][cury - 1].setxpyp(curx, cury);	// else set the node's parent to current node
+							n[curx][cury - 1].calch(stopx, stopy);	// recalculate the H score
+						}
+						break;
+					case 0:	// if the node is not in a list
+						n[curx][cury - 1].setstatus(1);	// place in open list
+						n[curx][cury - 1].calcg(n[curx][cury]);	// calculate the G score
+						n[curx][cury - 1].calch(stopx, stopy);	// calculate the H score
+						n[curx][cury - 1].setxpyp(curx, cury);	// set the node's parent to current node
+						break;
+				}
 			}
-		}	
+		}
 	}
-	print(n);
-	cout << n[curx][cury].getx() << "," << n[curx][cury].gety() << endl;
+
+	int path[SIZE*SIZE], pointer = 0;
+	int nextx, nexty;
+	curx = stopx;
+	cury = stopy;
+
+	while (curx != startx || cury != starty) {
+		path[pointer] = (n[curx][cury].getd() + 2)%4;
+		pointer++;
+		nextx = n[curx][cury].getxp();
+		nexty = n[curx][cury].getyp();
+		curx = nextx;
+		cury = nexty;
+	}
+
+	//print(n);
+	for (int i = pointer - 1; i >= 0; i--) {
+		cout << path[i] << ", ";
+	}
+	cout << endl;
 }
